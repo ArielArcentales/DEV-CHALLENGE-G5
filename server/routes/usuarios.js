2ac4f0673fa2d942bf59f883204b16c7afc37854
Ariel Arcentales <amarcentales@puce.edu.ec>
2025-07-03 21:56:50 -0500

    Rutas de usuarios funcionando

--- server/routes/usuarios.js
+++ server/routes/usuarios.js
@@ -1,1 +1,12 @@
 // Rutas para usuarios
+const express = require('express');
+const router = express.Router();
+const {
+  registrarUsuario,
+  obtenerUsuarios,
+} = require('../controllers/usuariosController');
+
+router.post('/', registrarUsuario);
+router.get('/', obtenerUsuarios);
+
+module.exports = router;
