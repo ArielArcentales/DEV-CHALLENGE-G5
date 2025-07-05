2babd4093f3fe2ef61093180f76fbc10f114eecb
Ariel Arcentales <amarcentales@puce.edu.ec>
2025-07-04 21:00:12 -0500

    Vista y lògica para el registro de usuarios

---
+++ front/js/main.js
@@ -0,0 +1,7 @@
+// js/main.js
+document.addEventListener('DOMContentLoaded', () => {
+  const yearSpan = document.getElementById('year');
+  if (yearSpan) {
+    yearSpan.textContent = new Date().getFullYear();
+  }
+});
